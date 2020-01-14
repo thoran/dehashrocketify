0e99cdc19ed6b30de91eb79fe79287d09676ea03
thoran <code@thoran.com>
2020-01-14 20:41:00 +1100

    0.8.0: Added the ability to handle symbols of the form :\w_\w and some weird edge case ones generally of the form :"" or :'' with or without _ or -.  Also added a Gemfile and Gemfile.lock.  Finally, I made it so that tests can be run from any directory.

--- test/dehashrocketify_test.rb
+++ test/dehashrocketify_test.rb
@@ -5,17 +5,28 @@ require 'fileutils'
 require 'minitest/autorun'
 require 'minitest-spec-context'
 
-describe '' do
+describe 'dehashrocketify' do
+
   before do
-    FileUtils.cp('in.rb', 'dir/test.rb')
-    system("ruby ../bin/dehashrocketify dir")
+    @dir_under_test = File.expand_path('./dir_under_test', __dir__)
+    program_path = File.expand_path('../bin/dehashrocketify', __dir__)
+
+    input_fixture_file_path = File.expand_path('./fixtures/in.rb', __dir__)
+    file_under_test_path = File.expand_path('./dir_under_test/test.rb', __dir__)
+
+    FileUtils.mkdir_p(@dir_under_test)
+    FileUtils.cp(input_fixture_file_path, file_under_test_path)
+    system("ruby #{program_path} #{@dir_under_test}")
   end
 
-  it '' do
-    expect(File.read('dir/test.rb') == File.read('out_compare.rb'))
+  it 'works' do
+    test_file_path = File.expand_path('./dir_under_test/test.rb', __dir__)
+    out_compare_file_path = File.expand_path('./fixtures/out_compare.rb', __dir__)
+
+    expect(File.read(test_file_path)).must_equal File.read(out_compare_file_path)
   end
 
   after do
-    FileUtils.rm('dir/test.rb')
+    FileUtils.rm_r(@dir_under_test)
   end
 end
---
+++ test/fixtures/out_compare.rb
@@ -0,0 +1,14 @@
+{
+  foo: 1,
+  bar!: 2,
+  baz?: 3,
+  foo_bar: 4,
+  "foobar": 5,
+  'foobar': 6,
+  "foo-bar": 7,
+  'foo-bar': 8,
+  "foo_bar": 9,
+  'foo_bar': 10,
+}
+
+SomeKindOf::Error => e
