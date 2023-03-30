87c60db0e900a0071a134d373bc5993d6819b350
thoran <code@thoran.com>
2023-03-31 10:53:14 +1100

    0.9.3: Fix ,:key => (Comma immediately in front of the colon.) case and added more tests.

--- test/fixtures/out_compare.rb
+++ test/fixtures/out_compare.rb
@@ -33,6 +33,28 @@
 { "foo_bar": 9 }
 { 'foo_bar': 10 }
 
+{foo:1}
+{bar!:2}
+{baz?:3}
+{foo_bar:4}
+{"foobar":5}
+{'foobar':6}
+{"foo-bar":7}
+{'foo-bar':8}
+{"foo_bar":9}
+{'foo_bar':10}
+
+{ foo:1 }
+{ bar!:2 }
+{ baz?:3 }
+{ foo_bar:4 }
+{ "foobar":5 }
+{ 'foobar':6 }
+{ "foo-bar":7 }
+{ 'foo-bar':8 }
+{ "foo_bar":9 }
+{ 'foo_bar':10 }
+
 (foo: 1)
 (bar!: 2)
 (baz?: 3)
@@ -43,6 +65,35 @@
 ('foo-bar': 8)
 ("foo_bar": 9)
 ('foo_bar': 10)
+
+( foo: 1 )
+( bar!: 2 )
+( baz?: 3 )
+( foo_bar: 4 )
+( "foobar": 5 )
+( 'foobar': 6 )
+( "foo-bar": 7 )
+( 'foo-bar': 8 )
+( "foo_bar": 9 )
+( 'foo_bar': 10 )
+
+{foo:1,bar:2}
+{foo:1, bar:2}
+{foo: 1,bar: 2}
+{foo: 1, bar: 2}
+{ foo:1,bar:2 }
+{ foo:1, bar:2 }
+{ foo: 1,bar: 2 }
+{ foo: 1, bar: 2 }
+
+(foo:1,bar:2)
+(foo:1, bar:2)
+(foo: 1,bar: 2)
+(foo: 1, bar: 2)
+( foo:1,bar:2 )
+( foo:1, bar:2 )
+( foo: 1,bar: 2 )
+( foo: 1, bar: 2 )
 
 SomeKindOf::Error => e
 
