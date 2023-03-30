c8330bdd69a8de6788a5390bcf638dd4f939d39a
thoran <code@thoran.com>
2023-03-30 21:06:02 +1100

    0.9.1: Fix {:key => value} case.

--- test/fixtures/out_compare.rb
+++ test/fixtures/out_compare.rb
@@ -11,6 +11,28 @@
   'foo_bar': 10,
 }
 
+{foo: 1}
+{bar!: 2}
+{baz?: 3}
+{foo_bar: 4}
+{"foobar": 5}
+{'foobar': 6}
+{"foo-bar": 7}
+{'foo-bar': 8}
+{"foo_bar": 9}
+{'foo_bar': 10}
+
+{ foo: 1 }
+{ bar!: 2 }
+{ baz?: 3 }
+{ foo_bar: 4 }
+{ "foobar": 5 }
+{ 'foobar': 6 }
+{ "foo-bar": 7 }
+{ 'foo-bar': 8 }
+{ "foo_bar": 9 }
+{ 'foo_bar': 10 }
+
 SomeKindOf::Error => e
 
 condition ? :first : :last => {}
